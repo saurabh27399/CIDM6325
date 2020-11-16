cbfa33bfa37efef3b9a94eff54dd33d8c1932be8
saurabh27399 <saurabh27399@gmail.com>
2020-11-15 23:49:10 -0600

    Chapter 9

--- myshopproject/myshop/cart/views.py
+++ myshopproject/myshop/cart/views.py
@@ -31,4 +31,14 @@ def cart_detail(request):
     for item in cart:
         item['update_quantity_form'] = CartAddProductForm(initial={'quantity': item['quantity'],
                                                                    'override': True})
-    return render(request, 'cart/detail.html', {'cart': cart})
+    coupon_apply_form = CouponApplyForm()
+
+    r = Recommender()
+    cart_products = [item['product'] for item in cart]
+    recommended_products = r.suggest_products_for(cart_products,
+                                                  max_results=4)
+
+    return render(request, 'cart/detail.html',
+                           {'cart': cart,
+                            'coupon_apply_form': coupon_apply_form,
+                            'recommended_products': recommended_products})
